70a5d2487736e1ab3f47b552bdc510b5b6a952dd
mahdi <mahdihellal@gmail.com>
2022-12-21 10:34:56 +0100

    ar button

--- src/models/ModelViewer.js
+++ src/models/ModelViewer.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "react-ar-viewer/dist/index.css";
 import ModelViewer from "react-ar-viewer";
 
 const ModelViewerPage = ({ modelPath, position = [0, 0, 0] }) => {
@@ -15,11 +15,15 @@ const ModelViewerPage = ({ modelPath, position = [0, 0, 0] }) => {
       alt={"Sample usage on component"}
       cameraControls={true}
       ar={true}
+      arModes={"webxr scene-viewer quick-look"}
+      arScale={"auto"}
       cameraTarget={"0m 0m 0m"}
       cameraOrbit={"0 deg 0deg 0%"}
       exposure={1}
       shadowSoftness={0}
       autoPlay={true}
+      autoRotate={true}
+      loading={"eager"}
     />
   );
 };
